db093fadcecb0722c30da4fbb981295778545a81
Nick Bircher <nicky.b.three@gmail.com>
2024-11-30 22:58:54 -0800

    Add non-functional block editor

---
+++ app/components/TipTap/hooks/useBlockEditor.ts
@@ -0,0 +1,62 @@
+import { useEditor } from '@tiptap/react'
+import type { Editor } from '@tiptap/core'
+
+import { ExtensionKit } from '@/extensions/extension-kit'
+
+declare global {
+  interface Window {
+    editor: Editor | null
+  }
+}
+
+const initialContent = {
+    type: 'doc',
+    content: [
+      {
+        type: 'heading',
+        attrs: {
+          textAlign: 'left',
+          level: 1,
+        },
+        content: [
+          {
+            type: 'text',
+            text: 'Create Documentation Here',
+          },
+        ],
+      },
+    ]
+}
+
+export const useBlockEditor = ({}: {}) => {
+
+  const editor = useEditor(
+    {
+      immediatelyRender: true,
+      shouldRerenderOnTransaction: false,
+      autofocus: true,
+      onCreate: ctx => {
+          ctx.editor.commands.setContent(initialContent)
+          ctx.editor.commands.focus('start', { scrollIntoView: true })
+        }
+      },
+      extensions: [
+        ...ExtensionKit({
+          provider,
+        }),
+      editorProps: {
+        attributes: {
+          autocomplete: 'off',
+          autocorrect: 'off',
+          autocapitalize: 'off',
+          class: 'min-h-full',
+        },
+      },
+    },
+    [ydoc, provider],
+  )
+
+  window.editor = editor
+
+  return { editor }
+}
