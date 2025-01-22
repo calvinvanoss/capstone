b23cba2304a8398ee62981c0d2111d744386a1c8
calvinvanoss <cvanoss@calpoly.edu>
2025-01-22 09:45:13 -0800

    add auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,17 +2,22 @@
 
 import { DashboardHeader } from '@/components/dashboard-header';
 import { ProjectList } from '@/components/project-list';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 export default function DashboardPage() {
-  const user = {
-    name: 'John Doe',
-    image: '/placeholder.svg?height=32&width=32',
-  };
-
   return (
-    <div className="container mx-auto px-4 py-8">
-      <DashboardHeader user={user} />
-      <ProjectList />
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="container mx-auto px-4 py-8">
+          <DashboardHeader user={user!} signOut={signOut!} />
+          <ProjectList />
+        </div>
+      )}
+    </Authenticator>
   );
 }
--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -1,20 +1,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@aws-amplify/ui-react';
+import { AuthUser } from 'aws-amplify/auth';
 
 type User = {
   name: string;
   image: string;
 };
 
-export function DashboardHeader({ user }: { user: User }) {
+export function DashboardHeader({
+  user,
+  signOut,
+}: {
+  user: AuthUser;
+  signOut: () => void;
+}) {
   return (
     <div className="flex justify-between items-center mb-8">
       <h1 className="text-3xl font-bold">Dashboard</h1>
       <div className="flex items-center space-x-4">
-        <span>{user.name}</span>
-        <Avatar>
-          <AvatarImage src={user.image} alt={user.name} />
-          <AvatarFallback>{user.name[0]}</AvatarFallback>
-        </Avatar>
+        <span>{user.username}</span>
+        <Button onClick={signOut}>Sign out</Button>
       </div>
     </div>
   );
