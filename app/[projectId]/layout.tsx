f4ea511b13d31bfdfa5c7531dc4325baeae66161
calvinvanoss <cvanoss@calpoly.edu>
2025-02-28 07:53:43 -0800

    partial dependency cleanup

--- app/[projectId]/layout.tsx
+++ app/[projectId]/layout.tsx
@@ -1,6 +1,5 @@
 import { ProjectHeader } from '@/components/project-header';
 import React from 'react';
-import DndWrapper from '@/components/dnd-wrapper';
 import { projectSchema } from '@/types/project';
 import { getProject } from '@/lib/server-actions';
 import { ProjectProvider } from '@/components/project-provider';
@@ -23,13 +22,11 @@ export default async function ProjectHomeLayout({
   const project = await getProject(parseInt(params.projectId));
 
   return (
-    <DndWrapper>
-      <ProjectProvider project={projectSchema.parse(project)}>
-        <div className="flex flex-col min-h-screen">
-          <ProjectHeader />
-          {children}
-        </div>
-      </ProjectProvider>
-    </DndWrapper>
+    <ProjectProvider project={projectSchema.parse(project)}>
+      <div className="flex flex-col min-h-screen">
+        <ProjectHeader />
+        {children}
+      </div>
+    </ProjectProvider>
   );
 }
--- auth.ts
+++ auth.ts
@@ -8,77 +8,80 @@ import { encode as defaultEncode } from 'next-auth/jwt';
 import { v4 as uuid } from 'uuid';
 
 const adapter = DrizzleAdapter(db);
-const providers: any[] = [GitHub];
 
-if (process.env.NODE_ENV === 'development') {
-  providers.push(
-    Credentials({
-      id: 'password',
-      name: 'Password',
-      credentials: {
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (credentials.password === 'password') {
-          const user = await db
-            .insert(users)
-            .values({
-              name: 'test user',
-              email: 'test@email.test',
-            })
-            .onConflictDoUpdate({
-              target: users.email,
-              set: { name: 'test user' },
-            })
-            .returning({
-              id: users.id,
-              name: users.name,
-              email: users.email,
-              image: users.image,
-              emailVerified: users.emailVerified,
-            });
+export const { handlers, auth, signIn, signOut } =
+  process.env.NODE_ENV === 'production'
+    ? NextAuth({
+        adapter,
+        providers: [GitHub],
+      })
+      // TODO: fix signin redirects
+    : NextAuth({
+        adapter,
+        providers: [
+          GitHub,
+          Credentials({
+            id: 'password',
+            name: 'Password',
+            credentials: {
+              password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials) {
+              if (credentials.password === 'password') {
+                const user = await db
+                  .insert(users)
+                  .values({
+                    name: 'test user',
+                    email: 'test@email.test',
+                  })
+                  .onConflictDoUpdate({
+                    target: users.email,
+                    set: { name: 'test user' },
+                  })
+                  .returning({
+                    id: users.id,
+                    name: users.name,
+                    email: users.email,
+                    image: users.image,
+                    emailVerified: users.emailVerified,
+                  });
 
-          return user[0] as User;
-        }
-        return null;
-      },
-    })
-  );
-}
+                return user[0] as User;
+              }
+              return null;
+            },
+          }),
+        ],
+        callbacks: {
+          async jwt({ token, account }) {
+            if (account?.type === 'credentials') {
+              token.credentials = true;
+            }
+            return token;
+          },
+        },
+        jwt: {
+          encode: async function (params) {
+            if (params.token?.credentials) {
+              const sessionToken = uuid();
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter,
-  providers,
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account?.type === 'credentials') {
-        token.credentials = true;
-      }
-      return token;
-    },
-  },
-  jwt: {
-    encode: async function (params) {
-      if (params.token?.credentials) {
-        const sessionToken = uuid();
+              if (!params.token.sub) {
+                throw new Error('No user ID found in token');
+              }
 
-        if (!params.token.sub) {
-          throw new Error('No user ID found in token');
-        }
+              const createdSession = await adapter?.createSession?.({
+                sessionToken: sessionToken,
+                userId: params.token.sub,
+                expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+              });
 
-        const createdSession = await adapter?.createSession?.({
-          sessionToken: sessionToken,
-          userId: params.token.sub,
-          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
-        });
+              if (!createdSession) {
+                throw new Error('Failed to create session');
+              }
 
-        if (!createdSession) {
-          throw new Error('Failed to create session');
-        }
-
-        return sessionToken;
-      }
-      return defaultEncode(params);
-    },
-  },
-});
+              return sessionToken;
+            }
+            return defaultEncode(params);
+          },
+        },
+      });
